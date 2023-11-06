8b4f2bcfc73815a7f162a6f98737011190f84e78
Jacob Lein <jacoble@stud.ntnu.no>
2023-11-06 12:34:14 +0100

    Update1

--- src/main/java/edu/ntnu/stud/TrainDispatch.java
+++ src/main/java/edu/ntnu/stud/TrainDispatch.java
@@ -1,5 +1,19 @@
 package edu.ntnu.stud;
 
-public class TrainDispatch {
-     
+import java.time.LocalTime;
+
+/**
+ * The train dispatch table.
+ * Information this class will involve, and their datatype:  
+ * Departure time(LocalTime), Line(String), Train number(int)
+ * Destination(String), Track(int), Delay(LocalTime)
+ * Immutable variables: Line, Train number, Destination, Track 
+ */
+public class TrainDispatch {        //Information regarding train departure
+  int trainNumber;
+  int track;
+  String destination;
+  LocalTime departureTime;
+  LocalTime delay;
+
 }
