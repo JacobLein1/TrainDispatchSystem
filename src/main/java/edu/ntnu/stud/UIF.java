bc8ffdce6e98d4a7595ee1506f7f33aa13010e17
Jacob Lein <jacoble@stud.ntnu.no>
2023-11-14 16:31:07 +0100

    Added javadoc and google style

--- src/main/java/edu/ntnu/stud/UIF.java
+++ src/main/java/edu/ntnu/stud/UIF.java
@@ -1,10 +1,14 @@
 package edu.ntnu.stud;
 
+/**.
+ * User interface class
+ */
 public class UIF {
-    public void start(){
-        System.out.println("Thread is running");
-    }
-    public void init(){
-        System.out.println("Initialised.");
-    }
+  public void start() {
+    System.out.println("Thread is running");
+  }
+    
+  public void init() {
+    System.out.println("Initialised.");  
+  }
 }
