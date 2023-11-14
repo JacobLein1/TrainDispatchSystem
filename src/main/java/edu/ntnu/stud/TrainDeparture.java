5798779a587e53ee40ceb265644cc586332967b0
Jacob Lein <jacoble@stud.ntnu.no>
2023-11-14 16:28:55 +0100

    Made throw new Illegal argument

--- src/main/java/edu/ntnu/stud/TrainDeparture.java
+++ src/main/java/edu/ntnu/stud/TrainDeparture.java
@@ -1,6 +1,9 @@
 package edu.ntnu.stud;
-import java.util.Objects;
+
 import java.time.LocalTime;
+import java.util.Objects;
+
+
 
 /**
  * The train dispatch table.
@@ -23,6 +26,27 @@ public class TrainDeparture {        //Information regarding train departure
   */
   public TrainDeparture(int trainNumber, int track, String line, 
       String destination, LocalTime departureTime, int delay) {
+    if (trainNumber < 0) {
+      throw new IllegalArgumentException("Train number must be positive. ");
+    }
+    if (track < 0) {
+      throw new IllegalArgumentException("Track must be a positive number. ");
+    }
+    if (line.equals(" ") || line.equals("")) {
+      throw new IllegalArgumentException("Destination has to be an input. ");
+    }
+    if (destination.equals(" ") || destination.equals("")) {
+      throw new IllegalArgumentException("Destination has to be an input. ");
+    } 
+    if (departureTime == null) {
+      throw new IllegalArgumentException("Must add a departure time. ");
+    }
+    if (delay < 0) {
+      throw new IllegalArgumentException("Delay must be positive.");
+      
+    }
+    //også hvis den er tom, 0, mellomrom, String
+
     this.trainNumber = trainNumber;
     this.track = track;
     this.line = line;
@@ -75,9 +99,9 @@ public class TrainDeparture {        //Information regarding train departure
 
     delay = addedDelayTime;
   }
-  //method to check if trainnumber already exists??
-  //compare to?? Fakk det?
-  public int compareTo(TrainDeparture otherDeparture){
+  //method to check if trainnumber already exists
+
+  public int compareTo(TrainDeparture otherDeparture) {
     return Integer.compare(this.getTrainNumber(), otherDeparture.getTrainNumber());
   }
 
