57035b57fc866be471ad25b51283b5ed146a42e4
Jacob Lein <jacoble@stud.ntnu.no>
2023-11-14 13:59:10 +0100

    Added JUnit testing

--- src/test/TestTrain.java
+++ src/test/TestTrain.java
@@ -1,13 +1,18 @@
+package edu.ntnu.stud;
 import org.junit.jupiter.api.Test;
+import org.junit.Assert.assertEqual;
+import org.junit.Assert.assertFalse;
+import org.junit.Assert.assertTrue;
 
 public class TestTrain {
-    Train train= new Train();
+    
+    
+    
     @Test
     void testTrack(){
-        if(train.getLine>0){
-        return true;            
-        }
-        return false;
+    LocalTime input = LocalTime.parse("22:46");
+    TrainDeparture oslo = new TrainDeparture(1,1,"L3","Bygdøy", input,2);
+    AssertEqual()
     }
     
 }
