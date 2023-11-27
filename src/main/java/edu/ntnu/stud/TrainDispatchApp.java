6bf0f660bef40ce8887054b6b8a1e1c8b6d6aa1c
Jacob Lein <jacoble@stud.ntnu.no>
2023-11-27 16:54:45 +0100

    Removed unnecessary code in TrainDispatchApp

--- src/main/java/edu/ntnu/stud/TrainDispatchApp.java
+++ src/main/java/edu/ntnu/stud/TrainDispatchApp.java
@@ -1,7 +1,6 @@
 package edu.ntnu.stud;
 
-import java.time.LocalTime;
-import java.util.Arrays;
+import edu.ntnu.stud.Models.TrainRegister;
 
 /**
  * This is the main class for the train dispatch application.
@@ -14,39 +13,12 @@ public class TrainDispatchApp {
    * 
    */
   public static void main(String[] args) {
-    UIF t1 = new UIF();
-    TrainRegister trainRegister = new TrainRegister(); 
-  
+    UserInterface t1 = new UserInterface();
+    
+    t1.init();
+
     t1.start();
 
-    t1.init();
-    LocalTime input = LocalTime.parse("22:46");
-    TrainDeparture barum = new TrainDeparture(2, 2, "L2",
-         "Hovik", input, LocalTime.parse("00:00"));
-
-    LocalTime input2 = LocalTime.parse("22:46");
-    TrainDeparture oslo = new TrainDeparture(1, 2, "L3", 
-        "Grønland", input2, LocalTime.parse("00:00"));
-    TrainDeparture bergen = new TrainDeparture(12, 1, "L3",
-         "Bygdøy", LocalTime.of(22, 30), LocalTime.parse("00:02"));
-    TrainDeparture tromso = new TrainDeparture(3, 1, "L3",
-         "Bygdøy", LocalTime.of(22, 30), LocalTime.parse("00:02"));
-
-
-    trainRegister.addNewTrainDeparture(oslo);
-    trainRegister.addNewTrainDeparture(bergen);
-    trainRegister.addNewTrainDeparture(tromso);
-    trainRegister.addNewTrainDeparture(barum);    
-    TrainDeparture stabekk = new TrainDeparture(2, 1, "L3",
-           "Bygdøy", LocalTime.of(22, 30), LocalTime.parse("00:02"));
-    TrainDeparture[] expected = {bergen, stabekk};
-    if (bergen.getDestination().equals(stabekk.getDestination())) {
-      System.out.println("Riktig");     
-    }
-
-    Utils utils = new Utils();
-
-    //System.out.println("skal kaste error");
-    //utils.parseStringToLocalTime(null);
-}
+   
+  }
 }
